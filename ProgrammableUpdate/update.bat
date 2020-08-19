d7ce58aeb6528699456fa6d49a8cc8e1a9cd9e42
Guohua Cao <caoguohua@gmail.com>
2020-08-19 10:37:08 -0400

    Updated 8-19-2020

--- ProgrammableUpdate/update.bat
+++ ProgrammableUpdate/update.bat
@@ -8,7 +8,7 @@ REM	4. Run this bat file, change the desfolder if necessary
 @echo on
 
 set destfolder=..
-
+copy /a top.html + index.html + bottom_index.html %destfolder%\index.html /b
 copy /a top.html + people.html + bottom_notindex.html %destfolder%\people.html /b
 copy /a top.html + positions.html + bottom_notindex.html %destfolder%\positions.html /b
 copy /a top.html + gallery.html + bottom_notindex.html %destfolder%\gallery.html /b
@@ -18,7 +18,7 @@ copy /a top.html + links.html + bottom_notindex.html %destfolder%\links.html /b
 copy /a top.html + teaching.html + bottom_notindex.html %destfolder%\teaching.html /b
 copy /a top.html + news.html + bottom_notindex.html %destfolder%\news.html /b
 copy /a top.html + intranet.html + bottom_notindex.html %destfolder%\intranet.html /b
-copy /a top.html + na.html + bottom_index.html %destfolder%\na.html /b
-copy /a top.html + index.html + bottom_index.html %destfolder%\index.html /b
+copy /a top.html + na.html + bottom_noindex.html %destfolder%\na.html /b
+copy /a top.html + ReadingList.html + bottom_noindex.html %destfolder%\ReadingList.html /b
 
 set destfolder=
